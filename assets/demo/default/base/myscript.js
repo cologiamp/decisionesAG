2d901c4055867ae2efd2b8968e1b4979ef83df6b
Ignacio Giampaoli <colo92@gmail.com>
2019-01-06 22:24:34 -0300

    Muchas modificaciones, muchaas

--- assets/demo/default/base/myscript.js
+++ assets/demo/default/base/myscript.js
@@ -4,10 +4,17 @@ function contenidoConfirmacion(elemento){
 	console.log(elemento.id);
 	//var valor = document.getElementById('m_maxlength_3').value;
 	//confirmacionDesc
+	if(elemento.value==''){
+		elemento.value = '-';
+	}
 	document.getElementById(elemento.id+'conf').innerHTML = elemento.value;
 }
 
 function contenidoVariablesConfirmacion(){
+
+	var texto1 = "<div class='m-form__heading'><h4 class='m-form__heading-title'>Detalle de variables</h4></div>";
+	var texto2;
+	var texto3 = '';
 	
 	for(i=1;i<=10;i++){
 		console.log(document.getElementById('variable'+i).className);
@@ -17,9 +24,17 @@ function contenidoVariablesConfirmacion(){
 			//RECOPILACION DE DATOS
 			// GET VARIABLE1COLOR.VALUE
 			var nombre = document.getElementById('variable'+i+'nombre').value;
-			var tipo = document.getElementById('variable'+i+'tipo').value;
+
+			var tipo = $('#variable'+i+'tipo').bootstrapSwitch('state');
+
+			if(tipo){
+				tipo = 'Continua';
+			}else{
+				tipo = 'Discreta';
+			}
+			//document.getElementById('variable'+i+'tipo').value;
 			var rango = document.getElementById('variable'+i+'rango').value;
-			var saltos = document.getElementsByName('variable'+i+'saltos').value;
+			var saltos = document.getElementById('variable'+i+'saltos').value;
 			var color = document.getElementById('variable'+i+'color').value;
 			//FIN RECOPILACION DE DATOS
 
@@ -28,6 +43,10 @@ function contenidoVariablesConfirmacion(){
 			console.log(rango);
 			console.log(saltos);
 			console.log(color);
+			
+			texto2 = "<div class='form-group m-form__group m-form__group--sm row'><label class='col-xl-4 col-lg-4 col-form-label' style='color: "+color+";'>"+nombre+":</label><div class='col-xl-8 col-lg-8'><span class='m-form__control-static'>"+tipo+", ("+rango+"), "+saltos+".</span></div></div>";
+
+			texto3 = texto3 + texto2;
 
 			//ASIGNACION DE DATOS
 			/*
@@ -40,5 +59,7 @@ function contenidoVariablesConfirmacion(){
 			//FIN ASIGNACION DE DATOS
 		}
 	}
+
+	document.getElementById('detalle-variables').innerHTML = texto1 + texto3;
 
 }
